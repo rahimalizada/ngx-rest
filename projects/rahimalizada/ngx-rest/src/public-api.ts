e99254a7126f18b1b65bf5de87a3108bee63b785
Rahim Alizada <rahim.alizada@gmail.com>
2020-05-09 01:02:05 +0400

    Fixed exported api list

--- projects/rahimalizada/ngx-rest/src/public-api.ts
+++ projects/rahimalizada/ngx-rest/src/public-api.ts
@@ -6,7 +6,7 @@ export * from './lib/model/pager/pager-request-filters-type.enum';
 export * from './lib/model/pager/pager-request.model';
 export * from './lib/model/pager/pager-result.model';
 export * from './lib/ngx-rest.module';
-export * from './lib/rest/abstract-get-all.resolver.spec';
+export * from './lib/rest/abstract-get-all.resolver';
 export * from './lib/rest/abstract-get-by-path.resolver';
 export * from './lib/rest/abstract-rest.service';
 export * from './lib/rest/date-parser.interceptor';
