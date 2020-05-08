a33a63bc0f1e2233597ecb623f980dc61b93a54e
Rahim Alizada <rahim.alizada@gmail.com>
2020-05-08 23:46:11 +0400

    Added imports and provider to library module

--- projects/rahimalizada/ngx-rest/src/lib/ngx-rest.module.ts
+++ projects/rahimalizada/ngx-rest/src/lib/ngx-rest.module.ts
@@ -1,8 +1,17 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { DateParserInterceptor } from './rest/date-parser.interceptor';
 
 @NgModule({
   declarations: [],
-  imports: [],
+  imports: [HttpClientModule],
   exports: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: DateParserInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class NgxRestModule {}
